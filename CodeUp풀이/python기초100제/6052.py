9d6f9486d4dd4587117f5e67e2d67d105dde00f6
SeungHyunKee <keeseunghyun@gmail.com>
2022-12-01 04:12:36 +0900

    221201

---
+++ CodeUp풀이/python기초100제/6052.py
@@ -0,0 +1,10 @@
+# 정수가 입력되었을 때, 
+# True/False 로 평가해주는 프로그램을 작성해보자
+
+n = int(input())
+print(bool(n))
+
+# bool( ) 을 이용하면 입력된 식이나 값을 평가해 
+# 불 형의 값(True 또는 False)을 출력
+
+# 입력된 값이 0이면 False, 0이 아니면 True 를 출력
