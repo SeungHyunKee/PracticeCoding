c19766cdb42f3e428e6510e49a10f2efd0484890
SeungHyunKee <keeseunghyun@gmail.com>
2022-12-14 03:38:20 +0900

    221212

--- examples/221207/ex03/3_2_face_eye_detect.py
+++ examples/221207/ex03/3_2_face_eye_detect.py
@@ -1,240 +1,65 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
+# python에서 OpenCV로 얼굴정렬
 
-def imshow(src, windowName='show', close=True):
-    cv2.imshow(windowName, src)
-    cv2.waitKey(0)
-    if close:
-        cv2.destroyAllWindows()
-
-
-img_ori = cv2.imread('car.png')
 '''
-channel : BGR
-height : height
-width : width
+1. 얼굴 및 눈 감지 위해 OpenCV Hear 캐스케이드 구성(정면얼굴 감지및 눈 감지 모듈)을 사용
+ face_casacade, eye_casacade 이 두 객체에 감지된 얼굴과 눈을 저장
+
+2.1 얼굴이미지 데이터 읽기
+2.2 얼굴이미지 바운딩 박스 : 케스케이드 경우 그레이스케일 이미지에서만 작동
+
+3. 눈 감지 : 사각형안에 위치할 두개의 관심영역 만들기
+ 1) 눈 감지할 회색조 이미지 영역, 2) 사각형 그릴 컬러이미지 필요
+
+4. left_eye , right_ eye
+
+5. 두 눈의 중심점 사이에 선 긋기, 그전에 직사각형 중심점 좌표 계산하기
+
+6. 수평선 그리고 그 선과 눈의 두 중심점을 연결하는 
+   선 사이의 각도 계산 : 최종목적=이 각도를 기준으로 이미지 회전시키는것
+
+7. 왼쪽눈 y 좌표가 오른쪽는 y좌표보다 크면 이미지를 시계방향으로 회전한다. 반대는 반시계방향
+
+8. 이미지를 각도 \theta 만큼 회전할수있음
+
+9. 이미지 크기 조정 : 눈 사이의 거리를 참조프레임으로 사용
 '''
 
-height, width, channel = img_ori.shape
-print(height, width, channel)
 
-# Convert Image to grayscale
-img_gray = cv2.cvtColor(img_ori, cv2.COLOR_BGR2GRAY)
+# 1. 얼굴 및 눈 감지 위해 OpenCV Hear 캐스케이드 구성(정면얼굴 감지및 눈 감지 모듈)을 사용
+#  >>> face_casacade, eye_casacade 이 두 객체에 감지된 얼굴과 눈을 저장
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-# Convolution Gaussian Filter
-img_blurred = cv2.GaussianBlur(img_gray, ksize=(5, 5), sigmaX=0)
+# 2.1 얼굴이미지 데이터 읽기 _ loading the image
+face_img = cv2.imread('face.png')
+face_gray = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
 
-img_blur_thresh = cv2.adaptiveThreshold(
-    img_blurred,
-    maxValue=255.0,
-    adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    thresholdType=cv2.THRESH_BINARY_INV,
-    blockSize=19,  # odd over 3
-    C=9
-)
+faces = face_cascade.detectMultiScale(face_gray, 1.1, 4)
 
-img_thresh = cv2.adaptiveThreshold(
-    img_gray,
-    maxValue=255.0,
-    adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    thresholdType=cv2.THRESH_BINARY_INV,
-    blockSize=19,  # odd over 3
-    C=9
-)
-plt.figure(figsize=(15, 15))
-img_type = ['gray', 'blur', 'orig_thresh', 'blur_thres']
-img_type_array = [img_gray, img_blurred, img_thresh, img_blur_thresh]
+# 2.2 얼굴이미지 바운딩 박스 >>> 케스케이드의 경우 그레이스케일 이미지에서만 작동
+for(x,y,w,h) in faces:
+    cv2.rectangle(face_img, (x,y), (x+w, y+h), (0,255,0), 3)
 
-# for idx, (name, image) in enumerate(zip(img_type, img_type_array)):
-#     plt.subplot(2, 2, idx+1)
-#     plt.imshow(image, 'gray')
-#     plt.title(name)
-# plt.tight_layout()
-# plt.show()
-
-contours, _ = cv2.findContours(
-    img_blur_thresh,
-    mode=cv2.RETR_LIST,
-    method=cv2.CHAIN_APPROX_SIMPLE
-)
-
-temp_result = np.zeros((height, width, channel), dtype=np.uint8)
-# cv2.drawContours(temp_result, contours=contours, contourIdx=-1, color=(0,255,0))
-# imshow(temp_result, 'temp_Result')
-
-contours_dict = []
-
-for i, contour in enumerate(contours):
-    x, y, w, h = cv2.boundingRect(contour)
-    contours_dict.append(
-        {
-            'contour': contour,
-            'x': x,
-            'y': y,
-            'w': w,
-            'h': h,
-            'cx': x + (w / 2),
-            'cy': y + (h / 2)
-        }
-    )
-    cv2.rectangle(temp_result, pt1=(x, y), pt2=(x + w, y + h), color=(255, 255, 255), thickness=2)
-    # cv2.imshow('temp_result', temp_result)
-    # cv2.waitKey(0)
-# imshow(temp_result)
-
-MIN_AREA = 80
-MIN_WIDTH, MIN_HEIGHT = 2, 8
-MIN_RATIO, MAX_RATIO = 0.25, 0.8
-
-possible_contours = []
-
-cnt = 0
-for d in contours_dict:
-    area = d['w'] * d['h']
-    ratio = d['w'] / d['h']
-
-    if area > MIN_AREA and d['w'] > MIN_WIDTH and d['h'] > MIN_HEIGHT and MIN_RATIO < ratio < MAX_RATIO:
-        d['idx'] = cnt
-        cnt += 1
-        possible_contours.append(d)
-
-temp_result = np.zeros((height, width, channel), dtype=np.uint8)
-
-for d in possible_contours:
-    cv2.rectangle(temp_result, (d['x'], d['y']), (d['x'] + d['w'], d['y'] + d['h']), (0, 255, 0))
-# imshow(temp_result, 'temp_result')
-
-MAX_DIAG_MULTIPLYER = 5
-MAX_ANGLE_DIFF = 12.0
-MAX_AREA_DIFF = 0.5
-MAX_WIDTH_DIFF = 0.8
-MAX_HEIGHT_DIFF = 0.2
-MIN_N_MATCHED = 3
-
-
-def find_chars(contour_list):
-    matched_result_idx = []
-    for d1 in contour_list:
-        matched_contour_idx = []
-        for d2 in contour_list:
-            if d1['idx'] == d2['idx']:
-                continue
-
-            dx = abs(d1['cx'] - d2['cx'])
-            dy = abs(d1['cy'] - d2['cy'])
-
-            diagonal_length1 = np.sqrt(d1['w'] ** 2 + d1['h'] ** 2)
-            # print(type(d1['cx']), type(d1['cy']) ,type(d2['cx']), type(d2['cy']))
-            distance = np.linalg.norm(np.array([d1['cx'], d1['cy']]) - np.array([d2['cx'], d2['cy']]))
-            if dx == 0:
-                angle_diff = 90
-            else:
-                angle_diff = np.degrees(np.arctan(dy / dx))
-
-            area_diff = abs(d1['w'] * d1['h'] - d2['w'] * d2['h']) / (d1['w'] * d1['h'])
-            width_diff = abs(d1['w'] - d2['w']) / d1['w']
-            height_diff = abs(d1['h'] - d2['h']) / d1['h']
-
-            if distance < diagonal_length1 * MAX_DIAG_MULTIPLYER \
-                    and angle_diff < MAX_ANGLE_DIFF and area_diff < MAX_AREA_DIFF \
-                    and width_diff < MAX_WIDTH_DIFF and height_diff < MAX_HEIGHT_DIFF:
-                matched_contour_idx.append(d2['idx'])
-
-        matched_contour_idx.append(d1['idx'])
-
-        if len(matched_contour_idx) < MIN_N_MATCHED:
-            continue
-
-        matched_result_idx.append(matched_contour_idx)
-        unmatched_contour_idx = []
-        for d4 in contour_list:
-            if d4['idx'] not in matched_contour_idx:
-                unmatched_contour_idx.append(d4['idx'])
-
-        unmatched_contour = np.take(possible_contours, unmatched_contour_idx)
-        recursive_contour_list = find_chars(unmatched_contour)
-
-        for idx in recursive_contour_list:
-            matched_result_idx.append(idx)
-
-        break
-    return matched_result_idx
-
-
-result_idx = find_chars(possible_contours)
-
-matched_result = []
-for idx_list in result_idx:
-    matched_result.append(np.take(possible_contours, idx_list))
-
-temp_result = np.zeros((height, width, channel), dtype=np.uint8)
-
-for r in matched_result:
-    for d in r:
-        cv2.rectangle(temp_result, (d['x'], d['y']), (d['x'] + d['w'], d['y'] + d['h']), (0, 255, 0), thickness=2)
-
-cv2.imshow('contours box', temp_result)
-cv2.waitKey(0)
-
-## Rotate plate image
-PLATE_WIDTH_PADDING = 1.3
-PLATE_HEIGHT_PADDING = 1.5
-MIN_PLATE_RATIO = 3
-MAX_PLATE_RATIO = 10
-
-plate_imgs = []
-plate_infos = []
-
-for i, matched_chars in enumerate(matched_result):
-    sorted_chars = sorted(matched_chars, key=lambda x: x['cx'])
-
-    plate_cx = (sorted_chars[0]['cx'] + sorted_chars[-1]['cx']) / 2
-    plate_cy = (sorted_chars[0]['cy'] + sorted_chars[-1]['cy']) / 2
-
-    plate_width = (sorted_chars[-1]['x'] + sorted_chars[-1]['w'] - sorted_chars[0]['x']) * PLATE_WIDTH_PADDING
-    sum_height = 0
-    for d in sorted_chars:
-        sum_height += d['h']
-
-    plate_height = int(sum_height / len(sorted_chars) * PLATE_HEIGHT_PADDING)
-    triangle_height = sorted_chars[-1]['cy'] - sorted_chars[0]['cy']
-    triangle_hypotenus = np.linalg.norm(
-        np.array([sorted_chars[0]['cx'], sorted_chars[0]['cy']]) -
-        np.array([sorted_chars[-1]['cx'], sorted_chars[-1]['cy']])
-    )
-    angle = np.degrees(np.arcsin(triangle_height / triangle_hypotenus))
-    rotation_matrix = cv2.getRotationMatrix2D(center=(plate_cx, plate_cy), angle=angle, scale=1.0)
-    img_rotated = cv2.warpAffine(img_thresh, M=rotation_matrix, dsize=(width, height))
-
-    img_cropped = cv2.getRectSubPix(
-        img_rotated,
-        patchSize=(int(plate_width), int(plate_height)),
-        center=(int(plate_cx), int(plate_cy))
-    )
-
-    if img_cropped.shape[1] / img_cropped.shape[0] < MIN_PLATE_RATIO or img_cropped.shape[1] / img_cropped.shape[
-        0] < MIN_PLATE_RATIO > MAX_PLATE_RATIO:
-        continue
-
-    x = int(plate_cx - plate_width/2)
-    y = int(plate_cy - plate_height / 2)
-    w = int(plate_width)
-    h = int(plate_height)
-    # num_idx = 0
-    for num_idx, sorted_char in enumerate(sorted_chars):
-        number_crop = cv2.getRectSubPix(
-            img_rotated,
-            patchSize=(int(sorted_char['w']), int(sorted_char['h'])),
-            center =(int(sorted_char['cx']), int(sorted_char['cy'])),
-        )
-        plt.subplot(1, len(sorted_chars), num_idx+1)
-        plt.imshow(number_crop, 'gray')
-    plt.show()
-
-    img_out = img_ori.copy()
-    cv2.rectangle(img_out, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
-    cv2.imshow('test', img_cropped)
-    cv2.imshow('orig', img_out)
+    roi_img = face_img[y:y+h, x:x+w]  # 원본 남겨놓음
+    roi_gray = face_gray[y:y+h, x:x+w] #
+    cv2.imshow('roi', roi_img)
     cv2.waitKey(0)
+
+# 3. 눈 감지 : 사각형안에 위치할 두개의 관심영역 만들기
+# roi = 관심영역
+# face 전체에서 관심영역만 빼옴
+
+eyes = eye_casacade.detectMultiScale(roi_gray, 1.1, 4)
+print(eyes)
+
+for(x,y,w,h) in eyes:
+    cv2.rectangle(roi_image, (x,y), (x+w, y+h), (0,255,0), 3)
+
+    roi_img = face_img[y:y+h, x:x+w] #이뒤에 .copy[] 해주지 않으면 원본이미지에 영향을 끼침
+    roi_gray = face_gray[y:y+h, x:x+w] 
+
+# cv2.imshow('face box', face_img)
+# cv2.waitKey(0)
