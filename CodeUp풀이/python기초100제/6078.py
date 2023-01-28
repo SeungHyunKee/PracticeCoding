66478efc0474e472e650abc2876cfecf1663216b
SeungHyunKee <keeseunghyun@gmail.com>
2023-01-28 18:56:10 +0900

    230128

---
+++ CodeUp풀이/python기초100제/6078.py
@@ -0,0 +1,20 @@
+# 영문 소문자 'q'가 입력될 때까지
+# 입력한 문자를 계속 출력하는 프로그램을 작성해보자
+
+# c = input()
+# while c != 'q':
+#     print('c', end = '\n')
+#     if c == 'q':
+#         break
+
+while True:
+    x = input()
+    print(x)
+    if x == 'q':
+        break
+
+
+    
+
+
+
